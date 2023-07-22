26ea942d697e33e7c0f10dcdca9a43772a595232
hanif365 <m.a.hanifkhan64@gmail.com>
2023-07-22 15:24:40 +0600

    make faq component dynamic

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -2,7 +2,28 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
-import { FaChevronDown, FaCaretDown } from "react-icons/fa6";
+import { FaCaretDown } from "react-icons/fa6";
+
+const faqData = [
+  {
+    id: 1,
+    title: "Title for Tab - 1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. ...1",
+  },
+  {
+    id: 2,
+    title: "Title for Tab - 2",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. ...2",
+  },
+  {
+    id: 3,
+    title: "Title for Tab - 3",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. ...3",
+  },
+];
 
 const Faq = () => {
   const [activeAccordion, setActiveAccordion] = useState(1);
@@ -11,137 +32,57 @@ const Faq = () => {
     setActiveAccordion((prevActive) => (prevActive === itemNo ? null : itemNo));
   };
 
-  const isAccordionOpen = (index) => {
-    return index === activeAccordion;
+  const isAccordionOpen = (itemNo) => {
+    return itemNo === activeAccordion;
   };
 
   return (
-    <div className="mt-60 text-center lg:px-20 2xl:px-60">
+    <div className="mt-60 lg:px-20 2xl:px-60">
       <div className="flex flex-col lg:flex-row justify-between">
         <div className="flex-1">
           {/* Accordion start */}
-          <div className="">
-            {/* below className e onek edid korsi */}
-            <div className="rounded overflow-hidden px-10 lg:px-20 lg:py-10">
-              {/* element 1 */}
-              <div
-                className={`group outline-none accordion-section `}
-                tabIndex="1"
-                onClick={() => toggleAccordion(1)}
-              >
-                <div className="group flex justify-between px-4 py-3 items-center transition ease duration-500 cursor-pointer pr-10 relative">
-                  <div
-                    className={`${
-                      isAccordionOpen(1)
-                        ? "text-blue-500 -rotate-180"
-                        : "text-gray-500"
-                    } h-8 w-8 rounded-full items-center inline-flex justify-center transform transition ease duration-500 absolute top-0 left-0 mb-auto ml-auto mt-2 mr-2`}
-                  >
-                    <FaCaretDown style={{ width: "40px", height: "40px" }} />
-                  </div>
-                  <div
-                    className={`${
-                      isAccordionOpen(1) ? "text-blue-500" : "text-gray-500"
-                    } font-bold transition ease duration-500 ml-6`}
-                  >
-                    Title for Tab - 1
-                  </div>
-                </div>
+          <div className="rounded overflow-hidden px-10 lg:px-20 lg:py-10">
+            {/* Render accordion items dynamically */}
+            {faqData.map((item) => (
+              <div key={item.id}>
                 <div
-                  className={`${
-                    isAccordionOpen(1) ? "max-h-screen" : ""
-                  }  max-h-0 px-4 overflow-hidden ease duration-500`}
+                  className={`group outline-none accordion-section `}
+                  tabIndex="1"
+                  onClick={() => toggleAccordion(item.id)}
                 >
-                  <p className="p-2 text-black text-justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Fugiat, repellat amet doloribus consequuntur eos similique
-                    provident tempora voluptates iure quia fuga dicta
-                    voluptatibus culpa mollitia recusandae delectus id suscipit
-                    labore?
-                  </p>
+                  <div className="group flex justify-between px-4 py-6 items-center transition ease duration-500 cursor-pointer pr-10 relative">
+                    <div
+                      className={`${
+                        isAccordionOpen(item.id)
+                          ? "text-blue-500 -rotate-180"
+                          : "text-gray-500"
+                      } h-8 w-8 rounded-full items-center inline-flex justify-center transform transition ease duration-500 absolute left-0 `}
+                    >
+                      <FaCaretDown style={{ width: "40px", height: "40px" }} />
+                    </div>
+                    <div
+                      className={`${
+                        isAccordionOpen(item.id)
+                          ? "text-blue-500"
+                          : "text-gray-500"
+                      } font-bold uppercase transition ease duration-500 ml-8`}
+                    >
+                      {item.title}
+                    </div>
+                  </div>
+                  <hr />
+                  <div
+                    className={`${
+                      isAccordionOpen(item.id) ? "max-h-screen" : ""
+                    }  max-h-0 px-4 overflow-hidden ease duration-500`}
+                  >
+                    <p className="p-2 text-black text-justify">
+                      {item.content}
+                    </p>
+                  </div>
                 </div>
               </div>
-              <hr />
-
-              {/* element 2 */}
-              <div
-                className={`group outline-none accordion-section `}
-                tabIndex="1"
-                onClick={() => toggleAccordion(2)}
-              >
-                <div className="group flex justify-between px-4 py-3 items-center transition ease duration-500 cursor-pointer pr-10 relative">
-                  <div
-                    className={`${
-                      isAccordionOpen(2)
-                        ? "text-blue-500 -rotate-180"
-                        : "text-gray-500"
-                    } h-8 w-8 rounded-full items-center inline-flex justify-center transform transition ease duration-500 absolute top-0 left-0 mb-auto ml-auto mt-2 mr-2`}
-                  >
-                    <FaCaretDown style={{ width: "40px", height: "40px" }} />
-                  </div>
-                  <div
-                    className={`${
-                      isAccordionOpen(2) ? "text-blue-500" : "text-gray-500"
-                    } font-bold transition ease duration-500 ml-6`}
-                  >
-                    Title for Tab - 2
-                  </div>
-                </div>
-                <div
-                  className={`${
-                    isAccordionOpen(2) ? "max-h-screen" : ""
-                  }  max-h-0 px-4 overflow-hidden ease duration-500`}
-                >
-                  <p className="p-2 text-black text-justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Fugiat, repellat amet doloribus consequuntur eos similique
-                    provident tempora voluptates iure quia fuga dicta
-                    voluptatibus culpa mollitia recusandae delectus id suscipit
-                    labore?
-                  </p>
-                </div>
-              </div>
-              <hr />
-              {/* element 3 */}
-              <div
-                className={`group outline-none accordion-section `}
-                tabIndex="1"
-                onClick={() => toggleAccordion(3)}
-              >
-                <div className="group flex justify-between px-4 py-3 items-center transition ease duration-500 cursor-pointer pr-10 relative">
-                  <div
-                    className={`${
-                      isAccordionOpen(3)
-                        ? "text-blue-500 -rotate-180"
-                        : "text-gray-500"
-                    } h-8 w-8 rounded-full items-center inline-flex justify-center transform transition ease duration-500 absolute top-0 left-0 mb-auto ml-auto mt-2 mr-2`}
-                  >
-                    <FaCaretDown style={{ width: "40px", height: "40px" }} />
-                  </div>
-                  <div
-                    className={`${
-                      isAccordionOpen(3) ? "text-blue-500" : "text-gray-500"
-                    } font-bold transition ease duration-500 ml-6`}
-                  >
-                    Title for Tab - 3
-                  </div>
-                </div>
-                <div
-                  className={`${
-                    isAccordionOpen(3) ? "max-h-screen" : ""
-                  }  max-h-0 px-4 overflow-hidden ease duration-500`}
-                >
-                  <p className="p-2 text-black text-justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Fugiat, repellat amet doloribus consequuntur eos similique
-                    provident tempora voluptates iure quia fuga dicta
-                    voluptatibus culpa mollitia recusandae delectus id suscipit
-                    labore?
-                  </p>
-                </div>
-              </div>
-              <hr />
-            </div>
+            ))}
           </div>
           {/* Accordion end */}
         </div>
