bd0a128bcc12fc39198374bdccd4075f5e17f0dd
hanif365 <m.a.hanifkhan64@gmail.com>
2023-08-07 21:43:29 +0600

    update UpcomingEvents text design

--- src/components/UpcomingEvents/UpcomingEvents.jsx
+++ src/components/UpcomingEvents/UpcomingEvents.jsx
@@ -5,22 +5,27 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
-import { FaLocationArrow, FaLocationDot } from "react-icons/fa6";
+import {
+  FaArrowRightLong,
+  FaDollarSign,
+  FaLocationArrow,
+  FaLocationDot,
+} from "react-icons/fa6";
+import { FaCalendarAlt } from "react-icons/fa";
 import Link from "next/link";
-import upcomingEventsData from '../../data/UpcomingEventsData.json';
+import upcomingEventsData from "../../data/UpcomingEventsData.json";
 
 const UpcomingEvents = () => {
-
   const settings = {
     dots: true,
     arrows: false,
     infinite: true,
     // pauseOnHover: false,
-    speed: 500,  // Animation speed in milliseconds
+    speed: 500, // Animation speed in milliseconds
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,   // Delay between each auto scroll (in milliseconds)
+    autoplaySpeed: 2000, // Delay between each auto scroll (in milliseconds)
     swipeToSlide: true,
     centerMode: true, // Enable center mode
     centerPadding: "0px", // Adjust padding to center the cards (60px for left and right showing)
@@ -71,12 +76,19 @@ const UpcomingEvents = () => {
                   className="w-full h-[20rem] 2xl:h-[24rem] group-hover:opacity-70 transition-all duration-500"
                 />
 
-                <div className="absolute mx-5 bottom-5 text-white group-hover:text-black text-left  transition-all duration-300  z-10">
-                  <p>{event.cost}</p>
-                  <div className="flex space-x-36 2xl:space-x-48">
-                    <div className="flex">
+                <div className="absolute px-5 bottom-5 text-white group-hover:text-black text-left transition-all duration-300 z-10 w-full">
+                  <div className="flex space-x-2">
+                    <FaDollarSign
+                      className="self-center text-yellow-500 group-hover:text-green-500 transition-all duration-300"
+                      style={{ width: "20px", height: "20px" }}
+                    />
+                    <p className="self-center ">{event.cost}</p>
+                  </div>
+                  {/* <div className="flex space-x-36 2xl:space-x-48"> */}
+                  <div className="flex justify-between">
+                    <div className="flex space-x-2">
                       <FaLocationDot
-                        className="self-center text-yellow-500"
+                        className="self-center text-blue-500"
                         style={{ width: "20px", height: "20px" }}
                       />
                       <p className="text-xl lg:text-2xl font-bold self-center">
@@ -84,14 +96,21 @@ const UpcomingEvents = () => {
                       </p>
                     </div>
                     <Link href={`/upcomingevents/${event.eventLocationLink}`}>
-                      <FaLocationArrow className="self-center text-yellow-500 invisible group-hover:visible cursor-pointer transition-all group-hover:delay-300 w-10 h-10 lg:w-14 lg:h-14" />
+                      <FaArrowRightLong className="self-center text-yellow-500 invisible group-hover:visible cursor-pointer transition-all group-hover:delay-300 w-6 h-6 lg:w-10 lg:h-10" />
+                      {/* <FaLocationArrow className="self-center text-yellow-500 invisible group-hover:visible cursor-pointer transition-all group-hover:delay-300 w-10 h-10 lg:w-14 lg:h-14" /> */}
                     </Link>
                   </div>
                   <div className="flex text-base  font-bold">
-                    <p className="">
-                      {event.date}{" "}
-                      <span className="ms-5 lg:ms-10">{event.days}</span>
-                    </p>
+                    <div className="flex space-x-2">
+                      <FaCalendarAlt
+                        className="self-center text-red-500"
+                        style={{ width: "20px", height: "20px" }}
+                      />
+                      <p className="">
+                        {event.date}
+                        <span className="ms-5 lg:ms-10">{event.days}</span>
+                      </p>
+                    </div>
                   </div>
                 </div>
 
