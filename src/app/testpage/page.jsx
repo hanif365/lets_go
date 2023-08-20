0aee283b39ca41c32cb10d5cc2c9e3aeda48c97c
hanif365 <m.a.hanifkhan64@gmail.com>
2023-08-20 18:50:25 +0600

    login page updated with testPage code and store login page code in testPage

--- src/app/testpage/page.jsx
+++ src/app/testpage/page.jsx
@@ -1,22 +1,10 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Link from "next/link";
+import { getProviders, signIn, useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Image from "next/image";
-import "./page.css";
-import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa6";
-import upcomingEventsData from "../../data/UpcomingEventsData.json";
-import { TypeAnimation } from "react-type-animation";
-import { signIn, useSession } from "next-auth/react";
+import Link from "next/link";
 
-const Login = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+const Login = ({ url }) => {
   const session = useSession();
   const router = useRouter();
   const params = useSearchParams();
@@ -49,264 +37,35 @@ const Login = () => {
     });
   };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-  // Function to check if the email is valid
-  const isEmailValid = (email) => {
-    return emailRegex.test(email);
-  };
-
-  console.log(error);
-
-  const settings = {
-    // dots: true,
-    arrows: false,
-    infinite: true,
-    fade: true,
-    pauseOnHover: false,
-    speed: 3000, // Animation speed in milliseconds
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 6000, // Delay between each auto scroll (in milliseconds)
-    swipeToSlide: true,
-    centerMode: true, // Enable center mode
-    centerPadding: "0px", // Adjust padding to center the cards (60px for left and right showing)
-  };
-
   return (
-    <div className="relative min-h-screen flex">
-      <div className="flex flex-col sm:flex-row-reverse items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 bg-white">
-        {/* image box start */}
-        <div className="sm:w-1/2 md:w-1/2 hidden md:block">
-          <Slider {...settings}>
-            {upcomingEventsData.map((event, index) => (
-              <div className="sm:w-1/2 md:w-2/5 h-full hidden md:flex flex-auto justify-center items-center  overflow-hidden text-white bg-no-repeat bg-cover relative  bg-gradient-to-r from-sky-500 to-indigo-400  rounded-bl-[10px] 2xl:rounded-none">
-                <div className="relative">
-                  <Image
-                    src={event.locationThumbnailImg}
-                    alt="upcoming event Image"
-                    width={2000}
-                    height={2000}
-                    className="w-full h-[110vh] 2xl:h-[100vh]"
-                  />
+    <div className="m-40">
+      <h1>{success ? success : "Welcome Back"}</h1>
+      <h2>Please sign in to see the dashboard.</h2>
 
-                  {/* when Image show uncomment below line */}
-                  <div className="absolute top-40 2xl:top-60 left-0 right-0 bottom-0 flex flex-col  px-10 z-[1]">
-                    {/* <div className="text-white flex flex-col  px-10 z-[1]"> */}
-                    <h1 className="text-2xl lg:text-4xl 2xl:text-6xl font-bold ">
-                      {/* Glad to see you! */}
-                      <TypeAnimation
-                        sequence={[
-                          "Begin Your Journey, Start Exploring, Feel Nature",
-                          1000,
-                          "Begin Your Journey, Start Exploring, Know Cultures",
-                          1000,
-                          "Begin Your Journey, Start Exploring, Enjoy Life",
-                          1000,
-                          "Begin Your Journey, Start Exploring, Create Stories",
-                          1000,
-                          "Begin Your Journey, Start Exploring, Find Harmony",
-                          1000,
-                        ]}
-                        speed={50}
-                        repeat={Infinity}
-                      />
-                    </h1>
-                    <p className="text-lg lg:text-xl 2xl:text-2xl font-bold pt-40">
-                      {event.title}
-                    </p>
-                  </div>
-                  {/* when Image show uncomment below line */}
-                  <div className="absolute top-0 left-0 right-0 bottom-0  bg-gradient-to-b from-blue-300 via-blue-500 to-blue-800 opacity-40"></div>
-                </div>
-                {/*  */}
-                <ul className="circles">
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                </ul>
-              </div>
-            ))}
-          </Slider>
-        </div>
-        {/* image box end */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <button className="text-green-600">Login</button>
+        <p className="text-red-600">{error && error}</p>
+      </form>
+      <button
+        onClick={() => {
+          signIn("google");
+        }}
+      >
+        Login with Google
+      </button>
 
-        <div className="md:flex md:items-center md:justify-center w-full sm:w-auto md:h-full md:w-1/2 p-8 md:p-10 lg:p-14 sm:rounded-lg md:rounded-none bg-white">
-          <div className="max-w-3xl w-full space-y-8 pt-20">
-            <div className="">
-              <h2 className="mt-6 text-3xl font-bold text-gray-900">
-                Welcome Back!
-              </h2>
-              <p className="mt-2 text-sm text-gray-500">
-                Please login to explore more
-              </p>
-            </div>
-            <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-              {/* email */}
-              <div className="relative">
-                {isEmailValid(formData.email) && (
-                  <div className="absolute right-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-10 w-8 text-green-500"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                      ></path>
-                    </svg>
-                  </div>
-                )}
-
-                <input
-                  className={`w-full text-base px-4 py-2 border-b border-gray-300 focus:outline-none rounded-2xl ${
-                    isEmailValid(formData.email)
-                      ? "focus:border-[#4C50E8]"
-                      : "focus:border-red-500"
-                  }`}
-                  type="text"
-                  name="email"
-                  placeholder="Email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-
-              {/* password */}
-              <div className="relative">
-                <input
-                  className="w-full text-base px-4 py-2 border-b border-gray-300 focus:outline-none rounded-2xl focus:border-[#4C50E8]"
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  value={formData.password}
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-
-              {/* other component */}
-
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember_me"
-                    name="remember_me"
-                    type="checkbox"
-                    className="h-4 w-4 bg-blue-500 focus:ring-blue-400 border-gray-300 rounded"
-                  />
-                  <label
-                    htmlFor="remember_me"
-                    className="ml-2 block text-sm text-gray-900"
-                  >
-                    Remember me
-                  </label>
-                </div>
-                <div className="text-sm">
-                  <a href="#" className="text-indigo-400 hover:text-blue-500">
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  // className="w-full register_btn relative flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-4 rounded-lg tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500"
-                  className="text-[18px] lg:text-[24px] register_btn w-full flex justify-center items-center px-10 py-8 relative border uppercase font-semibold  leading-none overflow-hidden bg-[#070B39] rounded-md text-white cursor-pointer tracking-[0.1em]"
-                >
-                  <span className="absolute inset-0 bg-yellow-400 rounded"></span>
-                  <span className="absolute inset-0 flex justify-center items-center font-bold">
-                    Login
-                  </span>
-                </button>
-              </div>
-              {error && <p className="text-red-500">{error}</p>}
-              {/* <p className="flex flex-col items-center justify-center mt-10 text-center text-md text-gray-500">
-                <span>Don't have an account?</span>
-                <a
-                  href="#"
-                  className="text-indigo-400 hover:text-blue-500 no-underline hover:underline cursor-pointer transition ease-in duration-300"
-                >
-                  Sign up
-                </a>
-              </p> */}
-            </form>
-
-            <p className="flex space-x-5  text-md text-gray-500">
-              <span>Don't have an account?</span>
-              <Link
-                href="/register"
-                className="text-indigo-400 hover:text-blue-500 no-underline hover:underline cursor-pointer transition ease-in duration-300"
-              >
-                Register
-              </Link>
-            </p>
-
-            <div className="flex items-center justify-center space-x-2">
-              <span className="h-px w-16 bg-gray-200"></span>
-              <span className="text-gray-300 font-normal">
-                or continue with
-              </span>
-              <span className="h-px w-16 bg-gray-200"></span>
-            </div>
-
-            <div className="flex flex-row justify-center items-center space-x-3">
-              <button
-                onClick={() => {
-                  signIn("google");
-                }}
-                className="w-11 h-11 items-center justify-center inline-flex rounded-2xl font-bold text-lg bg-blue-500 hover:shadow-lg cursor-pointer transition ease-in duration-300"
-              >
-                <FaGoogle className="w-6 h-6 text-white" />
-              </button>
-              <button
-                onClick={() => {
-                  signIn("github");
-                }}
-                className="w-11 h-11 items-center justify-center inline-flex rounded-2xl font-bold text-lg bg-blue-400 hover:shadow-lg cursor-pointer transition ease-in duration-300"
-              >
-                <FaGithub className="w-6 h-6 text-white" />
-              </button>
-
-              <button className="w-11 h-11 items-center justify-center inline-flex rounded-2xl font-bold text-lg bg-blue-900 hover:shadow-lg cursor-pointer transition ease-in duration-300">
-                <FaFacebookF className="w-6 h-6 text-white" />
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <button
+        onClick={() => {
+          signIn("github");
+        }}
+      >
+        Login with Github
+      </button>
+      <br />
+      <span>- OR -</span>
+      <Link href="/register" className="text-green-500">Create new account</Link>
     </div>
   );
 };
