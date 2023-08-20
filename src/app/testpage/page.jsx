527a9e5995af8b2e26f10fdc384eff9a5545e5ac
hanif365 <m.a.hanifkhan64@gmail.com>
2023-08-20 16:53:53 +0600

    update textPage code

--- src/app/testpage/page.jsx
+++ src/app/testpage/page.jsx
@@ -1,7 +1,7 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -10,48 +10,43 @@ import "./page.css";
 import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa6";
 import upcomingEventsData from "../../data/UpcomingEventsData.json";
 import { TypeAnimation } from "react-type-animation";
+import { signIn, useSession } from "next-auth/react";
 
-const Register = () => {
+const Login = () => {
   const [formData, setFormData] = useState({
-    name: "",
     email: "",
     password: "",
-    confirmPassword: "",
-    phoneNumber: "",
-    dateOfBirth: "",
-    address: "",
   });
-  const [error, setError] = useState(null);
-
+  const session = useSession();
   const router = useRouter();
+  const params = useSearchParams();
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
-  const handleSubmit = async (e) => {
+  console.log(session.status);
+
+  useEffect(() => {
+    setError(params.get("error"));
+    setSuccess(params.get("success"));
+  }, [params]);
+
+  if (session.status === "loading") {
+    return <p>Loading....</p>;
+  }
+
+  if (session.status === "authenticated") {
+    router?.push("/dashboard");
+  }
+
+  const handleSubmit = (e) => {
     e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
 
-    if (formData.password !== formData.confirmPassword) {
-      setError("Passwords do not match.");
-      return;
-    }
-
-    try {
-      const res = await fetch("/api/auth/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (res.status === 201) {
-        router.push("/login?success=Account has been created");
-      } else {
-        const data = await res.json();
-        setError(data.message || "Something went wrong!");
-      }
-    } catch (err) {
-      setError("Something went wrong!");
-      console.log(err);
-    }
+    signIn("credentials", {
+      email,
+      password,
+    });
   };
 
   const handleInputChange = (e) => {
@@ -67,11 +62,6 @@ const Register = () => {
   // Function to check if the email is valid
   const isEmailValid = (email) => {
     return emailRegex.test(email);
-  };
-
-  // Function to check if password and confirmPassword match
-  const doPasswordsMatch = () => {
-    return formData.password === formData.confirmPassword;
   };
 
   console.log(error);
@@ -99,14 +89,14 @@ const Register = () => {
         <div className="sm:w-1/2 md:w-1/2 hidden md:block">
           <Slider {...settings}>
             {upcomingEventsData.map((event, index) => (
-              <div className="sm:w-1/2 md:w-2/5 h-full hidden md:flex flex-auto justify-center items-center  overflow-hidden text-white bg-no-repeat bg-cover relative  bg-gradient-to-r from-sky-500 to-indigo-400  rounded-bl-[10px]">
+              <div className="sm:w-1/2 md:w-2/5 h-full hidden md:flex flex-auto justify-center items-center  overflow-hidden text-white bg-no-repeat bg-cover relative  bg-gradient-to-r from-sky-500 to-indigo-400  rounded-bl-[10px] 2xl:rounded-none">
                 <div className="relative">
                   <Image
                     src={event.locationThumbnailImg}
                     alt="upcoming event Image"
                     width={2000}
                     height={2000}
-                    className="w-full h-[155vh] 2xl:h-[125vh]"
+                    className="w-full h-[110vh] 2xl:h-[100vh]"
                   />
 
                   {/* when Image show uncomment below line */}
@@ -171,28 +161,13 @@ const Register = () => {
           <div className="max-w-3xl w-full space-y-8 pt-20">
             <div className="">
               <h2 className="mt-6 text-3xl font-bold text-gray-900">
-                Hello Travelers!
+                Welcome Back!
               </h2>
               <p className="mt-2 text-sm text-gray-500">
-                Please register to explore more
+                Please login to explore more
               </p>
             </div>
             <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-              {/* <input type="hidden" name="remember" value="true" /> */}
-
-              {/* name */}
-              <div className="relative">
-                <input
-                  className="w-full text-base px-4 py-2 border-b border-gray-300 focus:outline-none rounded-2xl focus:border-[#4C50E8]"
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                  value={formData.name}
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-
               {/* email */}
               <div className="relative">
                 {isEmailValid(formData.email) && (
@@ -242,82 +217,9 @@ const Register = () => {
                 />
               </div>
 
-              {/* confirm password */}
-              <div className="relative">
-                {formData.confirmPassword !== "" && doPasswordsMatch() && (
-                  <div className="absolute right-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-10 w-8 text-green-500"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      title="Password matched"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                      ></path>
-                    </svg>
-                  </div>
-                )}
-                <input
-                  className={`w-full text-base px-4 py-2 border-b border-gray-300 focus:outline-none rounded-2xl ${
-                    formData.confirmPassword !== "" && doPasswordsMatch()
-                      ? "focus:border-[#4C50E8]"
-                      : "focus:border-red-500"
-                  }`}
-                  type="password"
-                  name="confirmPassword"
-                  placeholder="Confirm Password"
-                  value={formData.confirmPassword}
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-
-              {/* Phone number */}
-              <div className="relative">
-                <input
-                  className="w-full text-base px-4 py-2 border-b border-gray-300 focus:outline-none rounded-2xl focus:border-[#4C50E8]"
-                  type="text"
-                  name="phoneNumber"
-                  placeholder="Phone Number"
-                  value={formData.phoneNumber}
-                  onChange={handleInputChange}
-                />
-              </div>
-
-              {/* DOB */}
-              <div className="relative">
-                <input
-                  className="w-full text-base px-4 py-2 border-b border-gray-300 focus:outline-none rounded-2xl focus:border-[#4C50E8]"
-                  type="date"
-                  name="dateOfBirth"
-                  placeholder="Date of Birth"
-                  value={formData.dateOfBirth}
-                  onChange={handleInputChange}
-                />
-              </div>
-
-              {/* Address */}
-              <div className="relative">
-                <textarea
-                  className="w-full text-base px-4 py-2 border-b bg-gray-50 border-gray-300 focus:outline-none rounded-2xl focus:border-[#4C50E8]"
-                  //   type="text"
-                  name="address"
-                  rows="4"
-                  placeholder="Address"
-                  value={formData.address}
-                  onChange={handleInputChange}
-                />
-              </div>
-
               {/* other component */}
 
-              {/* <div className="flex items-center justify-between">
+              <div className="flex items-center justify-between">
                 <div className="flex items-center">
                   <input
                     id="remember_me"
@@ -337,16 +239,16 @@ const Register = () => {
                     Forgot your password?
                   </a>
                 </div>
-              </div> */}
+              </div>
               <div>
                 <button
                   type="submit"
                   // className="w-full register_btn relative flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-4 rounded-lg tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500"
-                  className="text-[14px] lg:text-[16px] register_btn w-full flex justify-center items-center px-10 py-8 relative border uppercase font-semibold tracking-wider leading-none overflow-hidden bg-[#070B39] rounded-md text-white cursor-pointer"
+                  className="text-[18px] lg:text-[24px] register_btn w-full flex justify-center items-center px-10 py-8 relative border uppercase font-semibold  leading-none overflow-hidden bg-[#070B39] rounded-md text-white cursor-pointer tracking-[0.1em]"
                 >
                   <span className="absolute inset-0 bg-yellow-400 rounded"></span>
                   <span className="absolute inset-0 flex justify-center items-center font-bold">
-                    Register
+                    Login
                   </span>
                 </button>
               </div>
@@ -363,16 +265,16 @@ const Register = () => {
             </form>
 
             <p className="flex space-x-5  text-md text-gray-500">
-              <span>Already have an account?</span>
+              <span>Don't have an account?</span>
               <Link
-                href="/login"
+                href="/register"
                 className="text-indigo-400 hover:text-blue-500 no-underline hover:underline cursor-pointer transition ease-in duration-300"
               >
-                Log in
+                Register
               </Link>
             </p>
 
-            {/* <div className="flex items-center justify-center space-x-2">
+            <div className="flex items-center justify-center space-x-2">
               <span className="h-px w-16 bg-gray-200"></span>
               <span className="text-gray-300 font-normal">
                 or continue with
@@ -381,31 +283,27 @@ const Register = () => {
             </div>
 
             <div className="flex flex-row justify-center items-center space-x-3">
-              
-              <a
-                href=""
-                target="_blank"
+              <button
+                onClick={() => {
+                  signIn("google");
+                }}
                 className="w-11 h-11 items-center justify-center inline-flex rounded-2xl font-bold text-lg bg-blue-500 hover:shadow-lg cursor-pointer transition ease-in duration-300"
               >
                 <FaGoogle className="w-6 h-6 text-white" />
-              </a>
-              <a
-                href=""
-                target="_blank"
+              </button>
+              <button
+                onClick={() => {
+                  signIn("github");
+                }}
                 className="w-11 h-11 items-center justify-center inline-flex rounded-2xl font-bold text-lg bg-blue-400 hover:shadow-lg cursor-pointer transition ease-in duration-300"
               >
                 <FaGithub className="w-6 h-6 text-white" />
-              </a>
+              </button>
 
-              <a
-                href=""
-                target="_blank"
-                className="w-11 h-11 items-center justify-center inline-flex rounded-2xl font-bold text-lg bg-blue-900 hover:shadow-lg cursor-pointer transition ease-in duration-300"
-              >
+              <button className="w-11 h-11 items-center justify-center inline-flex rounded-2xl font-bold text-lg bg-blue-900 hover:shadow-lg cursor-pointer transition ease-in duration-300">
                 <FaFacebookF className="w-6 h-6 text-white" />
-              </a>
-              
-            </div> */}
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -413,4 +311,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
